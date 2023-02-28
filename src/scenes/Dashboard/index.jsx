9a12bd7b75f5a9b2706d1cd82d353e15997322e2
vladocpro <infernohs9@gmail.com>
2023-02-28 18:38:24 +0000

    Ready for deployment

--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {useDispatch,useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {fetchAuthMe,selectIsAuth} from "../../redux/slices/auth";
 
 const Dashboard = () => {
+   const isAuth = useSelector(selectIsAuth);
+
+   const authData= useSelector(state => state.auth)
+   const dispatch = useDispatch();
+
+   // const authData =  useSelector(state => state.auth.data)
+
+   let navigate = useNavigate()
+   const redirect = () => {
+      if(authData?.status == "loading") return
+      if(authData.data == null && !isAuth) navigate("/login")
+   }
+   useEffect(() => {
+      dispatch(fetchAuthMe())
+   },[])
+   useEffect(() => {
+      redirect()
+   },[authData])
    return (
        <div>
 
